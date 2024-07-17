129332997421adc4df23849a4fdd492d0d57568a
Ishan Chaturvedi <ishanchaturvedi25@gmail.com>
2024-07-17 16:11:30 +0530

    Added react-lottie dependency and implemented lottie animation in chat page.

--- client/src/pages/chat/index.jsx
+++ client/src/pages/chat/index.jsx
@@ -2,6 +2,9 @@ import { useAppStore } from "@/store";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "sonner";
+import ChatContainer from "./components/chat-container";
+import ContactsContainer from "./components/contacts-container";
+import EmptyChatContainer from "./components/empty-chat-container";
 
 const Chat = () => {
     const { userInfo } = useAppStore();
@@ -13,7 +16,11 @@ const Chat = () => {
         }
     }, [userInfo, navigate]);
     return (
-        <div>Chat</div>
+        <div className="flex h-[100vh] text-white overflow-hidden">
+            <ContactsContainer />
+            <EmptyChatContainer />
+            <ChatContainer />
+        </div>
     );
 };
 
