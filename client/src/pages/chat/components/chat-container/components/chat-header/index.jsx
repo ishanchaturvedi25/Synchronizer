4cb647580193a8fbb470b704712d95d113965e4a
ishanchaturvedi25 <ishanchaturvedi25@gmail.com>
2024-07-20 23:04:40 +0530

    Updated ChatHeader component to conditionally render avatar and display name based on selectedChatType.

--- client/src/pages/chat/components/chat-container/components/chat-header/index.jsx
+++ client/src/pages/chat/components/chat-container/components/chat-header/index.jsx
@@ -5,7 +5,6 @@ import { HOST } from "@/utils/constants";
 import { RiCloseFill } from "react-icons/ri";
 
 const ChatHeader = () => {
-
   const { closeChat, selectedChatData, selectedChatType } = useAppStore();
 
   return (
@@ -13,28 +12,50 @@ const ChatHeader = () => {
       <div className="flex gap-5 items-center w-full justify-between">
         <div className="flex gap-3 items-center justify-center">
           <div className="w-12 h-12 relative">
-            <Avatar className="h-12 w-12 rounded-full overflow-hidden">
-              {
-                selectedChatData.image ?
-                  <AvatarImage src={`${HOST}/${selectedChatData.image}`} alt="profile" className="object-cover w-full h-full bg-black" /> :
-                  <div className={`uppercase h-12 w-12 text-lg border-[1px] flex items-center justify-center rounded-full ${getColor(selectedChatData.color)}`}>
-                    {selectedChatData.firstName ? selectedChatData.firstName.split("").shift() : selectedChatData.email.split("").shift()}
+            {selectedChatType === "contact" ? (
+              <Avatar className="h-12 w-12 rounded-full overflow-hidden">
+                {selectedChatData.image ? (
+                  <AvatarImage
+                    src={`${HOST}/${selectedChatData.image}`}
+                    alt="profile"
+                    className="object-cover w-full h-full bg-black"
+                  />
+                ) : (
+                  <div
+                    className={`uppercase h-12 w-12 text-lg border-[1px] flex items-center justify-center rounded-full ${getColor(
+                      selectedChatData.color
+                    )}`}
+                  >
+                    {selectedChatData.firstName
+                      ? selectedChatData.firstName.split("").shift()
+                      : selectedChatData.email.split("").shift()}
                   </div>
-              }
-            </Avatar>
+                )}
+              </Avatar>
+            ) : (
+              <div className="bg-[#ffffff22] h-10 w-10 flex items-center justify-center rounded-full">
+                #
+              </div>
+            )}
           </div>
           <div>
-            {selectedChatType === "contact" && selectedChatData.firstName ? `${selectedChatData.firstName} ${selectedChatData.lastName}` : selectedChatData.email}
+            {selectedChatType === "channel" && selectedChatData.name}
+            {selectedChatType === "contact" && selectedChatData.firstName
+              ? `${selectedChatData.firstName} ${selectedChatData.lastName}`
+              : selectedChatData.email}
           </div>
         </div>
         <div className="flex items-center justify-center gap-5">
-          <button className="text-neutral-500 focus:border-none focus:outline-none focus:text-white duration-300 transition-all" onClick={closeChat}>
+          <button
+            className="text-neutral-500 focus:border-none focus:outline-none focus:text-white duration-300 transition-all"
+            onClick={closeChat}
+          >
             <RiCloseFill className="text-3xl" />
           </button>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ChatHeader
+export default ChatHeader;
