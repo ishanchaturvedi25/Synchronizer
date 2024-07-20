3fc2a5965272b2c2b4a231d7121d4204299da8de
ishanchaturvedi25 <ishanchaturvedi25@gmail.com>
2024-07-20 15:52:31 +0530

    Added file upload functionality to message bar, updated message container to display files, and created API routes for file upload and retrieval.

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -6,7 +6,7 @@ export const LOGIN_ROUTE = `${AUTH_ROUTES}/login`;
 export const GET_USER_INFO = `${AUTH_ROUTES}/user-info`;
 export const UPDATE_PROFILE_ROUTE = `${AUTH_ROUTES}/update-profile`;
 export const ADD_PROFILE_IMAGE_ROUTE = `${AUTH_ROUTES}/add-profile-image`;
-export const REMOVE_PROFILE_IMAGE_ROUTE= `${AUTH_ROUTES}/remove-profile-image`;
+export const REMOVE_PROFILE_IMAGE_ROUTE = `${AUTH_ROUTES}/remove-profile-image`;
 export const LOGOUT_ROUTE = `${AUTH_ROUTES}/logout`;
 
 export const CONTACTS_ROUTES = "api/contacts";
@@ -15,3 +15,4 @@ export const GET_DM_CONTACTS_ROUTES = `${CONTACTS_ROUTES}/get-contacts-for-dm`;
 
 export const MESSAGES_ROUTES = "api/messages";
 export const GET_ALL_MESSAGES_ROUTES = `${MESSAGES_ROUTES}/get-messages`;
+export const UPLOAD_FILE_ROUTE = `${MESSAGES_ROUTES}/upload-file`;
