921446206969d5494309279406dbe889e8360bd7
ishanchaturvedi25 <ishanchaturvedi25@gmail.com>
2024-07-20 12:42:50 +0530

    implemented the code to fetch previous messages.

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ import mongoose from "mongoose"
 import authRoutes from "./routes/AuthRoutes.js"
 import contactsRoutes from "./routes/ContactsRoutes.js"
 import setupSocket from "./socket.js"
+import messagesRoutes from "./routes/MessagesRoutes.js"
 
 dotenv.config();
 
@@ -26,6 +27,7 @@ app.use(express.json());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/contacts', contactsRoutes);
+app.use('/api/messages', messagesRoutes);
 
 const server = app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
