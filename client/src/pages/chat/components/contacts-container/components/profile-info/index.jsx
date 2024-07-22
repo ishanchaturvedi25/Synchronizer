b03fcebceb5a346acf8a6a14e55a9a0fec6d07a1
Ishan Chaturvedi <ishanchaturvedi25@gmail.com>
2024-07-22 16:53:59 +0530

    Modified logOut function to unselect chat when click on it.

--- client/src/pages/chat/components/contacts-container/components/profile-info/index.jsx
+++ client/src/pages/chat/components/contacts-container/components/profile-info/index.jsx
@@ -9,66 +9,68 @@ import { IoPowerSharp } from "react-icons/io5";
 import { useNavigate } from "react-router-dom";
 
 const ProfileInfo = () => {
-    const { userInfo, setUserInfo } = useAppStore();
+    const { userInfo, setUserInfo, setSelectedChatType, setSelectedChatData } = useAppStore();
     const navigate = useNavigate();
 
     const logOut = async () => {
         try {
             const response = await apiClient.post(LOGOUT_ROUTE, {}, { withCredentials: true });
 
-            if(response.status === 200) {
+            if (response.status === 200) {
                 navigate("/auth");
                 setUserInfo(null);
+                setSelectedChatType(undefined);
+                setSelectedChatData(undefined);
             }
         } catch (error) {
             console.log(error);
         }
     };
 
-  return (
-    <div className="absolute bottom-0 h-16 flex items-center justify-between px-10 w-full bg-[#2a2b33]">
-        <div className="flex gap-3 items-center justify-center">
-            <div className="w-12 h-12 relative">
-            <Avatar className="h-12 w-12 rounded-full overflow-hidden">
-                            {
-                                userInfo.image ? 
-                                <AvatarImage src={`${HOST}/${userInfo.image}`} alt="profile" className="object-cover w-full h-full bg-black"/> : 
+    return (
+        <div className="absolute bottom-0 h-16 flex items-center justify-between px-10 w-full bg-[#2a2b33]">
+            <div className="flex gap-3 items-center justify-center">
+                <div className="w-12 h-12 relative">
+                    <Avatar className="h-12 w-12 rounded-full overflow-hidden">
+                        {
+                            userInfo.image ?
+                                <AvatarImage src={`${HOST}/${userInfo.image}`} alt="profile" className="object-cover w-full h-full bg-black" /> :
                                 <div className={`uppercase h-12 w-12 text-lg border-[1px] flex items-center justify-center rounded-full ${getColor(userInfo.color)}`}>
                                     {userInfo.firstName ? userInfo.firstName.split("").shift() : userInfo.email.split("").shift()}
                                 </div>
-                            }
-                        </Avatar>
+                        }
+                    </Avatar>
+                </div>
+                <div>
+                    {
+                        userInfo.firstName && userInfo.lastName ? `${userInfo.firstName} ${userInfo.lastName}` : ""
+                    }
+                </div>
             </div>
-            <div>
-                {
-                    userInfo.firstName && userInfo.lastName ? `${userInfo.firstName} ${userInfo.lastName}` : ""
-                }
+            <div className="flex gap-5">
+                <TooltipProvider>
+                    <Tooltip>
+                        <TooltipTrigger>
+                            <FiEdit2 className="text-purple-500 text-xl font-medium" onClick={() => navigate("/profile")} />
+                        </TooltipTrigger>
+                        <TooltipContent className="bg-[#1c1b1e] border-none text-white">
+                            <p>Edit Profile</p>
+                        </TooltipContent>
+                    </Tooltip>
+                </TooltipProvider>
+                <TooltipProvider>
+                    <Tooltip>
+                        <TooltipTrigger>
+                            <IoPowerSharp className="text-red-500 text-xl font-medium" onClick={logOut} />
+                        </TooltipTrigger>
+                        <TooltipContent className="bg-[#1c1b1e] border-none text-white">
+                            <p>Logout</p>
+                        </TooltipContent>
+                    </Tooltip>
+                </TooltipProvider>
             </div>
         </div>
-        <div className="flex gap-5">
-            <TooltipProvider>
-                <Tooltip>
-                    <TooltipTrigger>
-                        <FiEdit2 className="text-purple-500 text-xl font-medium" onClick={() => navigate("/profile")}/>
-                    </TooltipTrigger>
-                    <TooltipContent className="bg-[#1c1b1e] border-none text-white">
-                    <p>Edit Profile</p>
-                    </TooltipContent>
-                </Tooltip>
-            </TooltipProvider>
-            <TooltipProvider>
-                <Tooltip>
-                    <TooltipTrigger>
-                        <IoPowerSharp className="text-red-500 text-xl font-medium" onClick={logOut}/>
-                    </TooltipTrigger>
-                    <TooltipContent className="bg-[#1c1b1e] border-none text-white">
-                    <p>Logout</p>
-                    </TooltipContent>
-                </Tooltip>
-            </TooltipProvider>
-        </div>
-    </div>
-  )
+    )
 }
 
 export default ProfileInfo
